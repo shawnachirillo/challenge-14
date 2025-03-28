2399047f3db88221ad47d7553cdeffbcaca2bd6d
Shawna Chirillo <schirillo711@gmail.com>
2025-03-28 13:19:47 -0500

    Initial commit

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,47 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+    return jwtDecode(this.getToken());
+
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+    // TODO: rurn a value that indicates if the user is logged in
+  }
+
+  isTokenExpired(token: string) {
+    // TODO: return a value that indicates if the token is expired
+    try {
+
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
+  getToken(): string {
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+  }
+
+  login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
+
+
+}
+
+export default new AuthService();
